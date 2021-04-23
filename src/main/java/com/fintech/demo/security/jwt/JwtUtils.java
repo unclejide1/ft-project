4edc8c1569bc6c73b2d0a184c29efc382d4e3e1a
Nwoke <nwoke@systemspecs.com.ng>
2021-04-23 09:40:19 +0100

    feat: begin security configuration

---
+++ src/main/java/com/fintech/demo/security/jwt/JwtUtils.java
@@ -0,0 +1,66 @@
+package com.fintech.demo.security.jwt;
+
+
+import com.fintech.demo.exceptions.BadRequestException;
+import com.fintech.demo.security.AuthenticatedUser;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+//import io.jsonwebtoken
+
+@Component
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${secured.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${secured.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        AuthenticatedUser userPrincipal = (AuthenticatedUser) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+            throw new BadRequestException("Sorry your token has an invalid jwt signature");
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+            throw new BadRequestException("Sorry your token is malformed");
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+            throw new BadRequestException("Sorry your token has expired");
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+
+
+}
---
+++ src/main/java/com/fintech/demo/security/services/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.fintech.demo.security.services;
+
+import com.fintech.demo.dao.UserDao;
+import com.fintech.demo.model.User;
+import com.fintech.demo.security.AuthenticatedUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public MyUserDetailsService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return AuthenticatedUser.build(user);
+    }
+}
